9acfdfd59fed84a0bc2cec2054b3fa10454cb9da
Hanna Yang <hannayangyang@gmail.com>
2019-05-30 15:00:08 +0800

    final

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,81 +1,174 @@
 import React, { Component } from 'react';
 import './App.css';
-import Quote from '../Components/Quote/Quote'; 
-import Buttons from '../Components/Buttons/Buttons'; 
 
-const quoteLibrary = [
-  {text: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.", author: 'Dr. Suess', color: '#16a085'}, 
-  {text: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.', author: 'Mark Twain', color: '#27ae60'}, 
-  {text: 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.', author: 'Mark Caine', color: '#2c3e50'}, 
-  {text: 'Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.', author: 'Lucille Ball', color: '#f39c12'}, 
-  {text: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.', author: 'Joshua J. Marine', color: '#e74c3c'},  
-  {text: 'Remember that the happiest people are not those getting more, but those giving more.', author: 'H. Jackson Brown, Jr.', color: '#9b59b6'}, 
-  {text: 'Live in the sunshine, swim the sea, drink the wild air.', author: 'Ralph Waldo Emerson', color: '#fb6964'},  
-  {text: 'We are what we repeatedly do; excellence, then, is not an act but a habit.', author: 'Aristotle', color: '#388894'}, 
-  {text: 'A man is a success if he gets up in the morning and gets to bed at night, and in between he does what he wants to do.', author: 'Bob Dylan', color: '#472e32'}, 
-  {text: 'The big lesson in life, baby, is never be scared of anyone or anything.', author: 'Frank Sinatra', color: '#94386C'}, 
-]; 
-
-const nextIndex = () => {
-  return Math.floor(Math.random() * quoteLibrary.length);
-};
-
-const constructTwitterUrl = {
-  preface: 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="', 
-  connection: '"', 
-  ending: '' }; 
-
-const constructTumblrUrl = {
-  preface: 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=', 
-  connection: '&content=', 
-  ending: '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
-};
-
+const priceList = {
+  salad: 1.20, 
+  bacon: 1.50, 
+  cheese: 1.40, 
+  meat: 1.60, 
+  bread: 2.0, 
+}
+  
 class App extends Component {
-  state = {
-    randomIndex: nextIndex(),
+  constructor(props) {
+    super(props); 
+    this.state = {
+      saladNum: 0, 
+      baconNum: 0, 
+      cheeseNum: 0, 
+      meatNum: 0, 
+    }; 
+    this.saladDecreaseHandler = this.saladDecreaseHandler.bind(this); 
+    this.saladIncreaseHandler = this.saladIncreaseHandler.bind(this); 
+    this.baconDecreaseHandler = this.baconDecreaseHandler.bind(this); 
+    this.baconIncreaseHandler = this.baconIncreaseHandler.bind(this); 
+    this.cheeseDecreaseHandler = this.cheeseDecreaseHandler.bind(this); 
+    this.cheeseIncreaseHandler = this.cheeseIncreaseHandler.bind(this); 
+    this.meatDecreaseHandler = this.meatDecreaseHandler.bind(this);
+    this.meatIncreaseHandler = this.meatIncreaseHandler.bind(this);
+    this.resetHandler = this.resetHandler.bind(this); 
   }; 
 
-  clickHandler = ( ) => {
-    this.setState ({
-      randomIndex: nextIndex(), 
-    }); 
-  };
+  saladDecreaseHandler() {
+    if(this.state.saladNum > 0) {
+      this.setState({
+        saladNum: this.state.saladNum - 1, 
+      })
+    }
+  }; 
+
+  saladIncreaseHandler() {
+    if(this.state.saladNum < 3) {
+      this.setState({
+        saladNum: this.state.saladNum + 1, 
+      })
+    }
+  }; 
+
+  baconDecreaseHandler() {
+    if(this.state.baconNum > 0) {
+      this.setState({
+        baconNum: this.state.baconNum - 1, 
+      })
+    }
+  }; 
+
+  baconIncreaseHandler() {
+    if(this.state.baconNum < 3) {
+      this.setState({
+        baconNum: this.state.baconNum + 1, 
+      })
+    }
+  }; 
+
+  cheeseDecreaseHandler() {
+    if(this.state.cheeseNum > 0) {
+      this.setState({
+        cheeseNum: this.state.cheeseNum - 1, 
+      })
+    }
+  }; 
+
+  cheeseIncreaseHandler() {
+    if(this.state.cheeseNum < 3) {
+      this.setState({
+        cheeseNum: this.state.cheeseNum + 1, 
+      })
+    }
+  }; 
+
+  meatDecreaseHandler() {
+    if(this.state.meatNum > 0) {
+      this.setState({
+        meatNum: this.state.meatNum - 1, 
+      })
+    }
+  }; 
+
+  meatIncreaseHandler() {
+    if(this.state.meatNum < 3) {
+      this.setState({
+        meatNum: this.state.meatNum + 1, 
+      })
+    }
+  }; 
+
+  resetHandler() {
+    this.setState({
+      saladNum: 0, 
+      baconNum: 0, 
+      cheeseNum: 0, 
+      meatNum: 0, 
+    })
+  }; 
+
+  getExtendedState(state) { 
+    let layers = [];
+    for(let i = 0; i < state.saladNum; i++) {
+      layers.push('salad')
+    }
+    for(let i = 0; i < state.baconNum; i++) {
+      layers.push('bacon')
+    } 
+    for(let i = 0; i < state.cheeseNum; i++) {
+      layers.push('cheese')
+    } 
+    for(let i = 0; i < state.meatNum; i++) {
+      layers.push('meat')
+    }
+    return { 
+      layers: layers, 
+      currentPrice: (2.0 + state.saladNum * priceList.salad + state.baconNum * priceList.bacon + state.cheeseNum * priceList.cheese + state.meatNum * priceList.meat).toFixed(2)
+    }
+  }; 
+
 
   render () {
-    const quoteDisplay = quoteLibrary[this.state.randomIndex]; 
-
-    const fontColorStyle = {
-      color: quoteDisplay.color
-    }; 
-
-    const backgroundStyle = {
-      backgroundColor: quoteDisplay.color
-    }; 
-
-    const twitterFullUrl = () => {
-      return constructTwitterUrl.preface + quoteDisplay.text + constructTwitterUrl.connection + quoteDisplay.author
-    }; 
-
-    const tublrFullUrl = () => {
-      return constructTumblrUrl.preface + quoteDisplay.author + constructTumblrUrl.connection + quoteDisplay.text + constructTumblrUrl.ending
-    }; 
-
+    const extendedState = this.getExtendedState(this.state);
+    const divLayers = extendedState.layers.map((className) => (<div className={'layer ' + className}></div>));
     return (
-      <div className="App" style = {backgroundStyle}>
-        <div style = {fontColorStyle}> 
-          <Quote 
-            text = {quoteDisplay.text}
-            author = {quoteDisplay.author}/>
-          <Buttons
-            twitterFullUrl = {twitterFullUrl()}
-            tublrFullUrl = {tublrFullUrl()}
-            clicked = {this.clickHandler}
-            buttonStyle = {backgroundStyle} />
+      <div className="App">
+        <nav>
+          <img className='logo' src='http://clipart-library.com/image_gallery/149336.png' alt='Burger Logo' /> 
+          <p>Welcome to Order Burger Online!</p>
+        </nav>
+        <div className='burger-builder'> 
+          <div className='layer top'></div>
+          {divLayers}
+          <div className='layer bottom'></div>
         </div>
+        <div className='current-price'>
+          <h3 id='current-price'> Current Price:&nbsp; <span className='price-num'>${extendedState.currentPrice}</span></h3>
+        </div>
+        <div className='ingredient-control'>
+          <div className='ingredient'>
+            <p className='ingredient-name'>Salad </p> 
+            <i onClick={this.saladDecreaseHandler} className="far fa-minus-square"></i> <span className='number'>{this.state.saladNum}</span> <i onClick={this.saladIncreaseHandler} className="far fa-plus-square"></i>
+          </div>
+          <div className='ingredient'>
+            <p className='ingredient-name'>Bacon </p> 
+            <i onClick={this.baconDecreaseHandler} className="far fa-minus-square"></i> <span className='number'>{this.state.baconNum}</span> <i onClick={this.baconIncreaseHandler} className="far fa-plus-square"></i>
+          </div>
+          <div className='ingredient'>
+            <p className='ingredient-name'>Cheese </p> 
+            <i onClick={this.cheeseDecreaseHandler} className="far fa-minus-square"></i> <span className='number'> {this.state.cheeseNum}</span> <i onClick={this.cheeseIncreaseHandler} className="far fa-plus-square"></i>
+          </div>
+          <div className='ingredient'>
+            <p className='ingredient-name'>Meat </p> 
+            <i onClick={this.meatDecreaseHandler} className="far fa-minus-square"></i> <span className='number'> {this.state.meatNum}</span> <i onClick={this.meatIncreaseHandler} className="far fa-plus-square"></i>
+          </div>
+        </div>
+        <div className='buttons'>
+          <button onClick={this.resetHandler}> Redo </button>
+          <button> Order </button>
+        </div>
+        <footer>
+          <p>Designed and coded by</p>
+          <p>Hanna Yang</p>
+        </footer>
       </div>
     );
   }; 
-}; 
+}
 
 export default App;
